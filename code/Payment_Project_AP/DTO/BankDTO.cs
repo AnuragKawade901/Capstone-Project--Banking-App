b5f92e6209f3a0114706fb0c0abdc369d78d42a3
AnuragKawade901 <kawade.anurag901@gmail.com>
2025-10-29 14:14:02 +0530

    bankdto

---
+++ code/Payment_Project_AP/DTO/BankDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Payment_Project_AP.DTO
+{
+    public class BankDTO
+    {
+        public string BankName { get; set; } = string.Empty;
+        public string IFSC { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+    }
+}
